37b05590a429aae039fb30e301c191d3d2a36c4a
Antonio Benavides Hernandez <g.antoniobh@gmail.com>
2020-09-20 15:37:36 -0600

    Fix

--- MailSender-Tarea1/MailSender-Tarea1/Form1.vb
+++ MailSender-Tarea1/MailSender-Tarea1/Form1.vb
@@ -70,23 +70,17 @@ Public Class Form1
 
 
     Public Function IsConnectionAvailable() As Boolean
-        ' Returns True if connection is available
-        ' Replace www.yoursite.com with a site that
-        ' is guaranteed to be online - perhaps your
-        ' corporate site, or microsoft.com
         Dim objUrl As New System.Uri("http://www.google.com/")
-        ' Setup WebRequest
+
         Dim objWebReq As System.Net.WebRequest
         objWebReq = System.Net.WebRequest.Create(objUrl)
         Dim objResp As System.Net.WebResponse
         Try
-            ' Attempt to get response and return True
             objResp = objWebReq.GetResponse
             objResp.Close()
             objWebReq = Nothing
             Return True
         Catch ex As Exception
-            ' Error, exit and return False
             objWebReq = Nothing
             Return False
         End Try
